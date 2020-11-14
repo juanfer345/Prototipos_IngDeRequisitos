7c66039b936c8d6cf45428ce415f7e44623854ac
juanfer345 <jfpatinoc@unal.edu.co>
2020-11-14 01:30:33 -0500

    vuelven los nombres caja para hacer pruebas de datos y que tales

--- Index.js
+++ Index.js
@@ -2925,67 +2925,276 @@ var profesores = {
     "Manuel Guisao": {
         nombre: "Manuel Guisao", correo: "manuel@gmail.com", celular: "3022839228", direccion: "Carrera 4 # 5-28", identificacion: "4"
     },
-    "Esteban Huanición": {
-        nombre: "Esteban Huanición", correo: "esteban@gmail.com", celular: "3103043829", direccion: "Calle 83 # 8 - 31", identificacion: "5"
+    "Esteban Huarnición": {
+        nombre: "Esteban Huarnición", correo: "esteban@gmail.com", celular: "3103043829", direccion: "Calle 83 # 8 - 31", identificacion: "5"
     }
 };
 
 var asignaturas = {
-
+    "Ingeniería de Software": {
+        contenidos: { contenido_1: "Esquema Preconceptual" },
+        estado: "Diseñada",
+        nombre: "Ingeniería de Software",
+        profesor: "Carlos Lopez"
+    },
+    "Programación Orientada a Objetos": {
+        contenidos: { contenido_1: "Clases", contenido_2: "Objetos" },
+        nombre: "Programación Orientada a Objetos",
+        profesor: "Daniel Delgado",
+        estado: "Diseñada"
+    },
+    "Ingeniería de Requisitos": {
+        contenidos: { contenido_1: "Casos de Uso", contenido_2: "Diagrama de Procesos" },
+        estado: "Diseñada",
+        nombre: "Ingeniería de Requisitos",
+        profesor: "Sara Berrio"
+    }
 };
 
 var clases = {
-
+    "Calidad del código": {
+        tematica: "Calidad del código",
+        numero: "1",
+        asignatura: "Programación Orientada a Objetos"
+    },
+    "Calidad de los casos de uso": {
+        tematica: "Calidad de los casos de uso",
+        numero: "1",
+        asignatura: "Ingeniería de Requisitos"
+    },
+    Scrum: {
+        tematica: "Scrum",
+        numero: "1",
+        asignatura: "Ingeniería de Software"
+    },
+    RUP: {
+        tematica: "RUP",
+        numero: "2",
+        asignatura: "Ingeniería de Software"
+    },
+    Cascada: {
+        tematica: "Cascada",
+        numero: "3",
+        asignatura: "Ingeniería de Software"
+    },
+    Espiral: {
+        tematica: "Espiral",
+        numero: "4",
+        asignatura: "Ingeniería de Software"
+    }
 };
-
 var criteriosInicial = {
-
+    "Aproximación a los requisitos iniciales": {
+        descripcion: "Lo expuesto por el estudiante se aproxima lo suficiente a los intereses del representante",
+        nombre: "Aproximación a los requisitos iniciales",
+        peso: "70",
+        rubrica: "Informe Inicial"
+    },
+    Plenitud: {
+        descripcion: "Se describen todas las características necesarias para los objetos, actores y funciones encontradas",
+        nombre: "Plenitud",
+        peso: "30",
+        rubrica: "Informe Inicial"
+    }
 };
-
 var criteriosProgreso = {
-
+    "Avance en los requisitos": {
+        descripcion: "Se nota el avance entre definir el contexto, analizar el problema y empezar a modelar la solución",
+        nombre: "Avance en los requisitos",
+        peso: "50",
+        rubrica: "Informe de Progreso"
+    },
+    Plenitud: {
+        descripcion: "Se describen y se asocian los objetivos y problemas, se establecen todos los procesos",
+        nombre: "Plenitud",
+        peso: "50",
+        rubrica: "Informe de Progreso"
+    }
 };
-
 var criteriosFinal = {
-
+    
 };
 var criteriosAlpha = {
-
+    "Funcionalidad": {
+        descripcion: "Todas las funciones estan correctas sin errores",
+        nombre: "Exito de funciones",
+        peso: "50",
+        rubrica: "Prototipo Alpha"
+    },
+    "Completitud": {
+        descripcion: "El prototipo tiene todo lo pedido por la interesada",
+        nombre: "Completitud",
+        peso: "50",
+        rubrica: "Prototipo Alpha"
+    }
 };
-
 var criteriosBeta = {
-
+    "Diseño": {
+        descripcion: "El prototipo tiene un buen diseño entendible y agradable a la vista",
+        nombre: "Diseño",
+        peso: "20",
+        rubrica: "Prototipo Beta"
+    },
+    "Funcionalidad": {
+        descripcion: "Todas las funciones estan correctas sin errores",
+        nombre: "Exito de funciones",
+        peso: "40",
+        rubrica: "Prototipo Beta"
+    },
+    "Completitud": {
+        descripcion: "El prototipo tiene todo lo pedido por la interesada",
+        nombre: "Completitud",
+        peso: "40",
+        rubrica: "Prototipo Beta"
+    }
 };
-
 var empresas = {
-
+    Postobon: {
+        mision: "Refrescar el mundo. Inspirar momentos de optimismo y felicidad. Crear valor y marcar la diferencia.",
+        necesidad: "Software para incrementar las ventas",
+        nombre: "Postobon",
+        objetivoEmpresa: "Crear y satisfacer la demanda. Generar rentabilidad y agregar valor a la organización.",
+        vision: "Satisfacer con excelencia a los consumidores de bebidas"
+    },
+    EPM: {
+        mision: "Somos una empresa filial del Grupo Empresarial EPM que contribuye al mejoramiento de la calidad de vida de la población a través de servicios de agua y energía con sustentabilidad ambiental.",
+        necesidad: "Aplicación para ubicación del personal en la ciudad",
+        nombre: "EPM",
+        objetivoEmpresa: "Mejorar los servicios básicos",
+        vision: "En 2022 Ticsa será una empresa líder en México en excelencia operativa, reputación y transparencia, ofreciendo a los clientes y al mercado un portafolio integral de soluciones hídricas y energéticas, fundamentada en prácticas socialmente responsables con todos los grupos de ínteres y contribuyendo a la consolidación multilatina del Grupo Empresarial EPM."
+    }
 };
-
 var representantes = {
-
+    "Santiago Bolaños": {
+        celular: "3123044398",
+        correo: "sbols@gmail.com",
+        disponibilidad: "Mucha",
+        nombre: "Santiago Bolaños",
+        empresa: "Postobon"
+    },
+    "Juan Perez": {
+        celular: "3023139870",
+        correo: "perez.juan@gmail.com",
+        disponibilidad: "Poca",
+        empresa: "EPM",
+        nombre: "Juan Perez"
+    }
 };
-
 var problemas = {
-
+    Postobon: {
+        actor: "Cliente",
+        causa: "Mala manipulación de la base de datos",
+        comentario: "Se hace muy difícil trabajar con un proyecto tan grande",
+        descripcionProblema: "Debido a un error en el software, muchas máquinas no proveen la bebida adecuada",
+        empresa: "Postobon",
+        impacto: "Alto",
+        proceso: "Obtención de bebidas",
+        validacion: "No Aprobado"
+    },
+    EPM: {
+        actor: "Administración",
+        causa: "Mala manipulación de la base de datos y resto del código",
+        comentario: "Buen problema para trabajar en el curso, pero se va a evitar la parte que involucra a los usuarios",
+        descripcionProblema: "No se puede acceder correctamente a la información y cuentas de algunos usuarios",
+        empresa: "EPM",
+        impacto: "Bajo",
+        proceso: "Obtención de información",
+        validacion: "Aprobado Con Ajustes"
+    }
 };
-
 var carteraDeProyectos = {
-    cantidadProyectos: 0,
-    proyectos: {}
+    cantidadProyectos: 1,
+    proyectos: {
+        EPM: {
+            descripcionProyecto: "Está bien dificil loks",
+            descripcionProyecto: "Es un buen proyecto, muy bien encaminado a la metodolgía de la asignatura",
+            empresa: "EPM",
+            objetivoProyecto: "Solucionar el problema"
+        }
+    }
 };
 
 var estudiantes = {
-
+    123: {
+        carrera: "Enfriación de Ladrillos",
+        celular: "456",
+        competencia: "Social",
+        correo: "elber@elber.com",
+        direccion: "Calle falsa 123",
+        historia: {
+            promedio: 3.5666666666666664,
+            asignaturasHist: {
+                nombre_1: { nombre_1: "Ladrillos I", nota_1: "2.3", semestre_1: "2015-I" },
+                nombre_2: { nombre_2: "Ladrillos II", nota_2: "3.4", semestre_2: "2016-III" },
+                nombre_3: { nombre_3: "Enfriación II", nota_3: "5", semestre_3: "2017" }
+            }
+        },
+        identificacion: "123",
+        nombre: "Elber Galarga",
+        semestre: "2020-III"
+    },
+    456: {
+        carrera: "Actuación Erotica",
+        celular: "89339",
+        competencia: "Técnica",
+        correo: "onlyfans.com/EstebitanBB",
+        direccion: "La vuelta negra",
+        historia: {
+            asignaturasHist: {
+                nombre_1: { nombre_1: "Poses I", nota_1: "5", semestre_1: "2013-I" },
+                nombre_2: { nombre_2: "Kamasutra IV", nota_2: "4.8", semestre_2: "2013-I" },
+                nombre_3: { nombre_3: "Perreo IX", nota_3: "9000", semestre_3: "2015-II" }
+            },
+            promedio: 3003.266666666667
+        },
+        identificacion: "456",
+        nombre: "Esteban Dido",
+        semestre: "el último"
+    }
 };
 
 var equipos = {
+    "EPM 1": {
+        cantidad: "1",
+        integrantes: {
+            estudiante_1: "123"
+        }
+    }
 
 };
 
 var metodologiasDesarrollo = {};
 
-var informesIniciales = {};
+var informesIniciales = {
+    "EPM 1": {
+        idea: "Mejorar las cosas que se puedan mejorar",
+        secciones: {
+            seccion_1: "Introducción",
+            seccion_2: "Índice",
+            seccion_3: "Capitulo I",
+            seccion_4: "Capitulo II",
+            seccion_5: "Conclusiones"
+        },
+        tema: "La Represa Hidroituango",
+        estado: "Incompleto",
 
+        rubrica: {
+            criterios: {
+                "Aproximación a los requisitos iniciales": {
+                    comentario: "melisimo",
+                    nota: "5",
+                    valoracion: "melo"
+                },
+                Plenitud: {
+                    comentario: "malísimo",
+                    nota: "1.23",
+                    valoracion: "malo"
+                }
+            },
+            nota: 3.8689999999999998
+        }
+    }
+};
 var informesProgreso = {};
 var informesFinales = {};
 var prototiposAlpha = {};
